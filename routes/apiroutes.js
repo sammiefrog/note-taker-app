6b63123692424b5348ec9060afc6b5cd7e8f30e2
sammiefrog <sammiefrog@live.com>
2020-04-18 16:52:29 -0500

    changed to promisifys

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,7 +1,11 @@
 const path = require("path");
 const fs = require("fs");
-const dbjson = require("../db/db.json");
+// const noteFile = fs.readFileSync("db.json");
+// const allTheNotes = JSON.parse(noteFile);
 const uuid = require('uuid');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 console.log(uuid.v4());
 
@@ -9,7 +13,12 @@ module.exports = app => {
 
     //get the notes when clicked
     app.get("/api/notes", (req, res) => {
-        return res.json(dbjson);
+        readFileAsync("../db/db.json", "utf8").then(data => {
+            let allTheNotes = JSON.parse(data)
+            console.log(allTheNotes)
+            return res.json(allTheNotes);
+        })
+
     });
 
     //post the notes
@@ -18,14 +27,21 @@ module.exports = app => {
         let id = uuid.v4();
         note.id = `${id}`;
 
-        dbjson.push(note);
-    
-        fs.writeFileSync("db.json", JSON.stringify(note), (err) => {
-            if (err) throw err;
-            console.log("success");
-        });
+        readFileAsync("../db/db.json", "utf8").then(data => {
+            let allTheNotes = JSON.parse(data);
+            allTheNotes.push(note);
 
-        res.json(note);
+            writeFileAsync("../db/db.json", JSON.stringify(note)).then(() => {
+                res.json(note);
+            })
+        })
+    
+        // fs.writeFileSync("../db/db.json", JSON.stringify(note), (err) => {
+        //     if (err) throw err;
+        //     console.log("success");
+        // });
+        // console.log(notes);
+        // res.json(note);
     });
     
     //delete the notes
