bf7149d8b80558f0a3c6816462b6c977a7453a2f
sammiefrog <sammiefrog@live.com>
2020-04-19 16:41:19 -0500

    comment

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,5 +1,6 @@
 const path = require('path');
 
+//routes for the two html pages
 const htmlRoutes = app => {
   app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
