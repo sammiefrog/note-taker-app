a8cf923efd465b13705ecfa33307e5b9bb36573a
sammiefrog <sammiefrog@live.com>
2020-04-20 14:40:14 -0500

    updated routes

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,11 +3,11 @@ const path = require('path');
 //routes for the two html pages
 const htmlRoutes = app => {
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"))
+    res.sendFile(path.join(__dirname, "../public/index.html"))
   });
 
   app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
   });
 }
 
--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
 
 //listening
 app.listen(PORT, () => {
